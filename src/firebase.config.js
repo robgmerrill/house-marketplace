b11c9b1f6f2fd078045fc5dd3b0877a5ed761e76
Rob Merrill <robgmerrill@gmail.com>
2023-04-20 18:15:15 -0700

    initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCS-XSGlV7P-GbdHzHOwuvBmRmCF1E_K8Q",
+  authDomain: "house-marketplace-app-752d8.firebaseapp.com",
+  projectId: "house-marketplace-app-752d8",
+  storageBucket: "house-marketplace-app-752d8.appspot.com",
+  messagingSenderId: "699144801550",
+  appId: "1:699144801550:web:f5861d24bdc6d31eaddb5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
---
+++ src/pages/Signin.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signin() {
+  return (
+    <div>
+      Sign In
+    </div>
+  )
+}
+
+export default Signin
